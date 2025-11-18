64ca029d78176d3c214a90bfae59035b0ed40494
AJM Fajlay Rabby <aadi4789@gmail.com>
2025-11-18 18:47:28 +0600

    problem 6

--- solution.ts
+++ solution.ts
@@ -83,3 +83,27 @@ const users = [
 ];
 
 console.log(filterActiveUsers(users));
+
+// Problem 6
+type Book = {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+    const availability = book.isAvailable ? "Yes" : "No";
+    console.log(
+        `Title: ${book.title}, Author: ${book.author}, Published Year: ${book.publishedYear}, Available: ${availability}`
+    );
+}
+
+const myBook: Book = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    publishedYear: 1925,
+    isAvailable: true,
+};
+
+printBookDetails(myBook);
