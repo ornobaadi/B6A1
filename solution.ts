9574e811b6e0ded2e27088ea208679f4ac30c1e6
AJM Fajlay Rabby <aadi4789@gmail.com>
2025-11-18 18:34:00 +0600

    problem 4

--- solution.ts
+++ solution.ts
@@ -45,3 +45,21 @@ console.log(person1.getDetails());
 
 const person2 = new Person('Alice', 25);
 console.log(person2.getDetails());
+
+
+// Problem 4
+type Item = {
+    title: string;
+    rating: number;
+};
+function filterByRating(items: Item[]): Item[] {
+    return items.filter((item) => item.rating >= 4);
+}
+
+const books = [
+    { title: 'Book A', rating: 4.5 },
+    { title: 'Book B', rating: 3.2 },
+    { title: 'Book C', rating: 5.0 },
+];
+
+console.log(filterByRating(books));
