cbc59198c91ff61a4648b35ade64ec5c33339d21
AJM Fajlay Rabby <aadi4789@gmail.com>
2025-11-18 18:19:07 +0600

    problem 2

--- solution.ts
+++ solution.ts
@@ -11,3 +11,16 @@ function formatValue(value: string | number | boolean): string | number | boolea
 console.log(formatValue('hello'));
 console.log(formatValue(5));
 console.log(formatValue(true));
+
+// Problem 2
+function getLength(value: string | any[]): number {
+    if (typeof value === "string") {
+        return value.length;
+    } else if (Array.isArray(value)) {
+        return value.length;
+    }
+    return 0;
+}
+
+console.log(getLength('typescript'));
+console.log(getLength([10, 20, 30, 40]));
